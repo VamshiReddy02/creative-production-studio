e484db3f55079e98c9fb0804d739a405969fc695
VamshiReddy02 <vamshiproject02@gmail.com>
2025-05-16 00:31:59 +0530

    minor changes
    Signed-off-by: VamshiReddy02 <vamshiproject02@gmail.com>

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
         <div className='flex justify-between items-center'>
             <img src={logoImg} alt="logo" className='h-9 md:h-[6rem] w-auto' />
             <button className='w-32 bg-black text-white border border-black h-13 rounded-full px-6 uppercase 
-                    text-md font-clash-display-general tracking-[0.2rem] hover:bg-orange-500 hover:text-white
-                     hover:border-orange-500 transition'>
+                    text-md font-clash-display-general tracking-[0.2rem] hover:bg-blue-500 hover:text-white
+                     hover:border-blue-500 transition'>
               menu
             </button>
         </div>
